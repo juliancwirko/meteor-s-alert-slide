01e4ac69ac4156072ab8b452152519d834a28c58
Julian Ćwirko <julian.cwirko@gmail.com>
2015-08-29 16:50:38 +0200

    updates

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     'summary': 'Slide effect for s-alert - simple and fancy notifications / alerts for Meteor.',
-    'version': '1.1.4',
+    'version': '3.0.0',
     'git': 'https://github.com/juliancwirko/meteor-s-alert-slide.git',
     'name': 'juliancwirko:s-alert-slide'
 });
 
 Package.onUse(function (api) {
-    api.use('juliancwirko:s-alert@2.4.2', ['client']);
-    api.imply('juliancwirko:s-alert@2.4.2', ['client']);
+    api.use('juliancwirko:s-alert@3.0.0', ['client']);
+    api.imply('juliancwirko:s-alert@3.0.0', ['client']);
     api.addFiles([
         's-alert-slide.css'
     ], 'client');
